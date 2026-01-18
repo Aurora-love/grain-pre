217117ea4d550a8c3befad703242d5b7bcb95d4d
Aurora-love <1930633129@qq.com>
2026-01-18 16:45:31 +0800

    lasted

---
+++ src/core/events/Event.h
@@ -0,0 +1,51 @@
+#pragma once
+#include "core/Core.h"
+namespace GE {
+
+enum class EventType {
+    None = 0,
+    WindowClose, WindowResize, WindowFocus, WindowLostFocus, WindowMoved,
+    KeyPressed, KeyReleased, KeyTyped,
+    MouseButtonPressed, MouseButtonReleased, MouseMoved, MouseScrolled
+};
+enum EventCategory {
+    None = 0,
+    EventCategoryApplication = BIT(0),
+    EventCategoryInput       = BIT(1),
+    EventCategoryKeyboard    = BIT(2),
+    EventCategoryMouse       = BIT(3),
+    EventCategoryMouseButton = BIT(4)
+};
+#define EVENT_CLASS_TYPE(type) static EventType GetStaticType() { return EvnetType::type; }\
+                                virtual EventType GetEventType() const override { return GetStaticType(); }\
+                                virtual const char* GetName() const override { return #type; }
+#define EVENT_CLASS_CATEGORY(category) virtual int GetCategoryFlags() const override { return category; }
+
+class Event {
+public:
+    virtual ~Event() = default;
+    bool m_IsHandled = false;
+    virtual EventType GetEventType() const = 0;
+    virtual const char* GetName() const = 0;
+    virtual int GetCategoryFlags() const = 0;
+    inline bool IsInCategory(EventCategory category) { return GetCategoryFlags() & category; }
+};
+
+class EventDispatcher {
+    template<typename T>
+    using EventCalledFn = std::function<bool(T&)>;
+
+public:
+    EventDispatcher(Event& event) : m_Event(event) {}
+    template<typename T>
+    bool Dispatch(EventCalledFn<T> func) {
+        if(m_Event.GetEventType() == T::GetStaticType()) {
+            m_Event.m_IsHandled = func(static_cast<T&>(m_Event));
+            return true;
+        }
+        return false;
+    }
+private:
+    Event& m_Event;
+};
+}
